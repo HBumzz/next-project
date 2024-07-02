033b71a2f916ce5dbb3fbcfd3bcfcb610e341b17
HBumzz <choi12920@gmail.com>
2024-07-02 11:25:01 +0900

    FEAT : page start

--- components/misc/ButtonOutline..js
+++ components/misc/ButtonOutline..js
@@ -4,7 +4,7 @@ const ButtonOutline = ({ children }) => {
   return (
     <button className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange ">
       {" "}
-      {children}
+      <div>010-0000-0000</div>
     </button>
   );
 };
