ad0a15b359949d2bcd01bcaf83fe49957e9a71d7
HBumzz <choi12920@gmail.com>
2024-07-03 16:05:46 +0900

    FIX: font size

--- components/Consulting.js
+++ components/Consulting.js
@@ -20,7 +20,7 @@ const Cunsulting = () => {
               <h3 className="text-2xl lg:text-4xl font-medium text-black-600 text-left">
                 고민 말고 신청하세요.
               </h3>
-              <div className="pt-3 text-l lg:text-sm text-gray-400 font-medium text-left">
+              <div className="pt-3 text-sm lg:text-lg text-gray-400 font-medium text-left">
                 문의 하시면 최대한 빠르게 연락드리겠습니다.
               </div>
             </motion.h3>
@@ -46,13 +46,16 @@ const Cunsulting = () => {
                           alt="Free Plan"
                         />
                       </div>
-                      <div className="my-2 text-[12px] md:text-md font-medium text-center">
+                      <div className="my-2 text-[12px] lg:text-lg font-medium text-center">
                         카카오톡 문의
                       </div>
                     </div>
                   </div>
                 </a>
-                <a href="tel:+821021548941" className="button-link">
+                <a
+                  href="tel:+821021548941"
+                  className="button-link hover:underline"
+                >
                   <div className="flex justify-center px-2 md:px-12">
                     <div className="grid-row-2 items-center ">
                       <div className="relative w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 flex items-center justify-center mx-auto">
@@ -63,13 +66,16 @@ const Cunsulting = () => {
                           alt="Free Plan"
                         />
                       </div>
-                      <div className="my-2 text-[12px] md:text-md font-medium text-center">
+                      <div className="my-2 text-[12px] lg:text-lg font-medium text-center">
                         전화 문의
                       </div>
                     </div>
                   </div>
                 </a>
-                <a href="sms:+821021548941" className="button-link">
+                <a
+                  href="sms:+821021548941"
+                  className="button-link hover:underline"
+                >
                   <div className="flex justify-center px-2 md:px-12">
                     <div className="grid-row-2 items-center ">
                       <div className="relative w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 flex items-center justify-center mx-auto">
@@ -80,7 +86,7 @@ const Cunsulting = () => {
                           alt="Free Plan"
                         />
                       </div>
-                      <div className="my-2 text-[12px] md:text-md font-medium text-center">
+                      <div className="my-2 text-[12px] lg:text-lg font-medium text-center">
                         문자메세지 문의
                       </div>
                     </div>
@@ -90,7 +96,7 @@ const Cunsulting = () => {
                   href="https://t.me/+M5jO9KziK6UyZWE1"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="button-link"
+                  className="button-link hover:underline"
                 >
                   <div className="flex justify-center px-2 md:px-12">
                     <div className="grid-row-2 items-center ">
@@ -102,7 +108,7 @@ const Cunsulting = () => {
                           alt="Free Plan"
                         />
                       </div>
-                      <div className="my-2 text-[12px] md:text-md font-medium text-center">
+                      <div className="my-2 text-[12px] lg:text-lg font-medium text-center">
                         텔레그램 문의
                       </div>
                     </div>
